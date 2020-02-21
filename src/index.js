b836dc44738e8f87e0d86bf133ad6be0f217c84e
katherinChi <jrodriguezruidias@gmail.com>
2020-02-21 08:42:10 -0500

    jrodriguezruidias@gmail.com

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
 import validator from './validator.js';
 
-console.log(validator);
+let contenedor1 =document.getElementById("contenedor1");
+let contenedor2 =document.getElementById("contenedor2");
+const texto = document.getElementById('texto');
+const name = document.getElementById('name');
+const btn=document.getElementById("btn");
+const btn2=document.getElementById("btn2");
+contenedor2.style.display="none";
+contenedor3.style.display="none";
+
+
+const botonera  = (event) => {
+	event.preventDefault();
+		contenedor1.style.display="none";
+		contenedor2.style.display="block";
+		validator.isValid(name.value);
+		texto.innerHTML = `Tu tarjeta ${String(validator.enmascarar(name.value))} ha sido ingresada`;
+};
+
+
+ const regresar = (event)=> {
+	 event.preventDefault();
+	contenedor2.style.display="none";
+	contenedor1.style.display="block";
+				
+};
+
+btn.addEventListener('click', botonera);
+btn2.addEventListener('click', regresar);
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,54 @@
+const validar=(name) =>{
+    const strReverse= name.split("").reverse();
+	let suma=0;
+	const arr = [];
+	for (let i = 0; i <=strReverse.length-1; i++){
+		if(i%2!==0){
+			const n=parseInt(strReverse[i])
+			const doble = n * 2
+			if(doble > 9) {
+				const division=doble%10+1
+				suma+=parseInt(division);
+			} else {
+				suma +=parseInt(doble);
+			}
+		} else {
+		    suma+=parseInt(strReverse[i]);
+		}
+	}
+	if(suma%10===0){
+		console.log(true);
+	} else {
+		console.log(false);
+	}
+}
+
+const maskify = (name) => {
+	let ocultar="";
+	if (name.length <=4){
+		return name
+	}else{
+		const charsToRemove = name.length - 4;
+		let sliced = name.slice(charsToRemove, );
+		for (let i=0; i<=name.length; i++){
+			ocultar+="#"; 
+		const modulo=ocultar.concat(sliced);
+		return modulo
+		}
+    }
+}
+
 const validator = {
-  // ...
+
+	isValid: (name)=>{
+	return	validar(name);
+	},
+
+    enmascarar: (name) =>{
+	 return   maskify(name);	  
+	}
+	
 };
+		
 
 export default validator;
